5d0d07ef60a9e66d377c5b94e2442bf9a01418d7
r61boff <r61boff@mail.ru>
2018-06-09 21:03:31 +0200

    Обновлен проект: доавблены файлы заданий с решением из другого репозитория

---
+++ task1.js
@@ -0,0 +1,7 @@
+/* Task 1 */
+
+/* Put your code here */
+var str=process.argv[2], str2;
+str=str.replace(/[!.?,:;\n\s]/g,"").toLowerCase();
+str2=str.split("").reverse().join("");
+process.stdout.write((str===str2)?"YES":"NO");
---
+++ task2.js
@@ -0,0 +1,6 @@
+/* Task 2 */
+
+/* Put your code here */
+var day=process.argv[2], month=process.argv[3], year=process.argv[4], mounth_in_year = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+var day_last = String((new Date(year, 11, 31) - new Date(year, mounth_in_year.indexOf(month), day))/86400000);
+process.stdout.write(day_last);
