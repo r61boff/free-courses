74883eac118066010c051f89b9b1e8f3eb24edb2
r61boff <r61boff@mail.ru>
2018-06-17 11:39:10 +0200

    исправлено отображение для цифр 11: часов, минут, секунд

--- task3.js
+++ task3.js
@@ -8,16 +8,16 @@ min = Math.floor((t_sum%3600)/60);
 sec = t_sum%60;
 if (t_sum !== 0){
 if (hour > 0) {
-	if ((hour%10) === 1) {time_result+=hour+' час ';}
+	if (((hour%10) === 1)&&((hour%100) !== 11)) {time_result+=hour+' час ';}
 	else if (((hour%10) > 1)&&((hour%10) < 5)) {time_result+=hour+' часа ';}
 	else {time_result+=hour+' часов ';}
 }
 if (min > 0) {
-	if ((min%10) === 1) {time_result+=min+' минута ';}
+	if (((min%10) === 1)&&(min !== 11)) {time_result+=min+' минута ';}
 	else if (((min%10) > 1)&&((min%10) < 5)) {time_result+=min+' минуты ';}
 	else {time_result+=min+' минут ';}
 }
-	if ((sec%10) === 1) {time_result+=sec+' секунда';}
+	if (((sec%10) === 1)&&(sec !== 11)) {time_result+=sec+' секунда';}
 	else if (((sec%10) > 1)&&((sec%10) < 5)) {time_result+=sec+' секунды';}
 	else {time_result+=sec+' секунд';}
 
